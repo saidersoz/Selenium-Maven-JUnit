038f825d1fd3ff808bb1474d96a1103552a5fcf4
saidErsoz <m.said.ersoz3443@gmail.com>
2023-03-28 00:55:06 +0300

    28/03/2023

--- src/test/java/Day06/C01_MavenIlkTest.java
+++ src/test/java/Day06/C01_MavenIlkTest.java
@@ -1,8 +1,17 @@
 package Day06;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
 public class C01_MavenIlkTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
          /*
          https://www.amazon.com/ sayfasına gidelim
@@ -13,8 +22,56 @@ public class C01_MavenIlkTest {
          Sayfadaki tüm başlıkları yazdıralım
           */
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
 
+        //https://www.amazon.com/ sayfasına gidelim
+        driver.get("https://www.amazon.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Thread.sleep(1000);
 
+        //Arama kutusunu locate edelim
+        WebElement search = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+
+        //“Samsung headphones” ile arama yapalım
+        search.sendKeys("Samsung headphones");
+        search.submit();
+        Thread.sleep(1000);
+
+        //Bulunan sonuç yazısını yazdıralım
+        WebElement resultFull = driver.findElement(By.xpath("(//div[@class='sg-col-inner'])[1]"));
+        String result = resultFull.getText();
+        String[] resultArr = result.split(" ");
+        System.out.println("Toplam " + resultArr[2] + " Sonuç Bulundu");
+        Thread.sleep(1000);
+
+        //İlk ürünü tıklayalım
+        //Tüm arama sonucu ürünleri list içine koyup ilk elemana tıklıyoruz
+        List<WebElement> firstProduct = driver.findElements(By.xpath("(//img[@class='s-image'])"));
+        firstProduct.get(0).click();
+        Thread.sleep(1000);
+
+        //Sayfadaki tüm başlıkları yazdıralım
+        System.err.println("İlk Ürün Başlıkları ");
+        List<WebElement> titles = driver.findElements(By.xpath("//h1"));
+        for (WebElement each : titles) {
+            System.out.println("==> "+each.getText());
+        }
+        Thread.sleep(1000);
+
+        System.out.println();
+        //bir önce ki sayfaya gelelim
+        driver.navigate().back();
+        System.err.println("Ana Sayfa Başlıkları ");
+        List<WebElement> anaTitles = driver.findElements(By.xpath("//h2"));
+        for (WebElement each : anaTitles){
+            System.out.println("==> "+each.getText());
+        }
+        Thread.sleep(1000);
+
+        //sayfayı kapatalım
+        driver.close();
     }
 
 }
--- src/test/java/Day06/C02_MavenClassWork.java
+++ src/test/java/Day06/C02_MavenClassWork.java
@@ -4,7 +4,18 @@ public class C02_MavenClassWork {
 
     public static void main(String[] args) {
 
-
+         /*
+         http://zero.webappsecurity.com sayfasına gidin
+         Signin buttonuna tıklayın
+         Login alanına  “username” yazdırın
+         Password alanına “password” yazdırın
+         Sign in buttonuna tıklayın
+         Pay Bills sayfasına gidin
+         Amount kısmına yatırmak istediğiniz herhangi bir miktarı yazın
+         Date kısmına “2023-03-27” yazdırın
+         Pay buttonuna tıklayın
+         “The payment was successfully submitted.” mesajının çıktığını kontrol edin
+          */
 
     }
 
