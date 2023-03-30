b98eeed85fdfb9c3090b64c173d22c0fdc1237a8
saidErsoz <m.said.ersoz3443@gmail.com>
2023-03-30 18:07:34 +0300

    30/03/2023

--- src/test/java/ODEV_Selenium/Test02.java
+++ src/test/java/ODEV_Selenium/Test02.java
@@ -1,5 +1,17 @@
 package ODEV_Selenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
 public class Test02 {
 
       /*
@@ -12,4 +24,59 @@ public class Test02 {
       - Sayfayı kapatınız
        */
 
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(2500);
+        driver.close();
+    }
+
+    @Test
+    public void test01() throws InterruptedException {
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        Thread.sleep(1000);
+        WebElement addElement = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
+        for (int i = 1; i < 101; i++) {
+            addElement.click();
+        }
+        Thread.sleep(1000);
+
+        List<WebElement> deleteKacAdet = driver.findElements(By.xpath("//button[@onclick='deleteElement()']"));
+        int count = 0;
+        for (WebElement each : deleteKacAdet) {
+            count++;
+        }
+        if (count == 100) {
+            System.out.println("Add Element Tuşuna 100 Kere Basıldı");
+        } else {
+            System.out.println("Add Element Tuşuna " + count + " Kere Basıldı");
+        }
+        Thread.sleep(1000);
+
+        List<WebElement> delete = driver.findElements(By.xpath("//button[@class='added-manually']"));
+        for (int i = 0; i < 90; i++) {
+            delete.get(i).click();
+        }
+        Thread.sleep(1000);
+
+        List<WebElement> deleteSonHali = driver.findElements(By.xpath("//button[@class='added-manually']"));
+        int sayac = 0;
+        for (WebElement each : deleteSonHali) {
+            sayac++;
+        }
+        if (sayac == 10) {
+            System.out.println("Delete Tuşuna 90 Kere Basıldı");
+        } else {
+            System.out.println("Delete Tuşuna" + sayac + "Kere Basıldı");
+        }
+    }
 }
