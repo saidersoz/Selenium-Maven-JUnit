38ae235ca8ddbd960ae96d3777edb0df301bd463
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-05 23:22:08 +0300

    05/04/2023

--- src/test/java/ODEV_Selenium/Task06.java
+++ src/test/java/ODEV_Selenium/Task06.java
@@ -18,9 +18,34 @@ public class Task06 extends TestBase {
 
 
     @Test
-    public void task06() {
+    public void task06() throws InterruptedException {
         driver.get("https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/");
-        driver.switchTo().frame("emoojis");
-        driver.findElement(By.xpath("(//span[@data-upgraded=',MaterialRipple'])[2]")).click(); bekle(2);
+        driver.switchTo().frame(1);
+
+        WebElement element = driver.findElement(By.xpath("(//*[@class='mdl-tabs__ripple-container mdl-js-ripple-effect'])[2]"));
+        element.click(); Thread.sleep(1500);
+
+        WebElement emojiler2 = driver.findElement(By.xpath("//div[@id='nature']"));
+        System.out.println(emojiler2.getText()); Thread.sleep(1500);
+
+        driver.switchTo().parentFrame();
+
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[1]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[2]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[3]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[4]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[5]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[6]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[7]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[8]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[9]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[10]")).sendKeys("said");
+        driver.findElement(By.xpath("(//input[@class='mdl-textfield__input'])[11]")).sendKeys("said");
+        Thread.sleep(2500);
+
+        driver.findElement(By.xpath("//button[@class='mdl-button mdl-js-button mdl-button--raised mdl-button--colored']")).click();
+
+        WebElement text = driver.findElement(By.xpath("//div[@class='mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded']"));
+        System.out.println(text.getText());
     }
 }
--- src/test/java/ODEV_Selenium/test3a.java
+++ src/test/java/ODEV_Selenium/test3a.java
@@ -1,7 +1,7 @@
 package ODEV_Selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.*;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -10,75 +10,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class Test03 {
-
-    /*
-    -BeforeClass methodu ile driver ı olusturun ve Maximize edin, 15 sn bekletin
-    -Before methodunda http://www.google.com adresine gidin
-    -Üç farklı test methodu ile;
-    -arama kutusuna "The God Father" yazip, cikan sonuc sayisini yazdirin
-    -arama kutusuna  "Lord of the Rings" yazip, cikan sonuc sayisini yazdirin
-    -arama kutusuna  "Kill Bill" yazip, cikan sonuc sayisini yazdirin
-    -NOT: Çıkan sonuç sayılarını After methodunda yazdırınız
-    -AfterClass ile kapatın
-     */
-    static WebDriver driver;
-
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        driver.get("http://www.google.com");
-    }
+public class test3a {
 
     @Test
     public void test01() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("http://www.google.com");
+
         //-arama kutusuna "The God Father" yazip, cikan sonuc sayisini yazdirin
         WebElement firstSearchBox = driver.findElement(By.xpath("(//textarea)[1]"));
         firstSearchBox.sendKeys("The God Father" + Keys.ENTER);
         Thread.sleep(2000);
-    }
-
-    @Test
-    public void test02() throws Exception {
-        //-arama kutusuna  "Lord of the Rings" yazip, cikan sonuc sayisini yazdirin
-        WebElement secondsSearchBox = driver.findElement(By.xpath("(//textarea)[1]"));
-        secondsSearchBox.sendKeys("Lord of the Rings" + Keys.ENTER);
-        Thread.sleep(2000);
-    }
-
-    @Test
-    public void test03() throws Exception {
-        //-arama kutusuna  "Kill Bill" yazip, cikan sonuc sayisini yazdirin
-        WebElement thirdSearchBox = driver.findElement(By.xpath("(//textarea)[1]"));
-        thirdSearchBox.sendKeys("Kill Bill" + Keys.ENTER);
-        Thread.sleep(2000);
-    }
-
-    @After
-    public void tearDown() throws Exception {
         WebElement result1 = driver.findElement(By.xpath("//div[@id='result-stats']"));
         String[] resultArr1 = result1.getText().split(" ");
         System.out.println("The God Father ==> " + resultArr1[1] + " Sonuç Bulundu");
 
+        //-arama kutusuna  "Lord of the Rings" yazip, cikan sonuc sayisini yazdirin
+        WebElement secondsSearchBox = driver.findElement(By.xpath("(//textarea)[1]"));
+        secondsSearchBox.sendKeys("Lord of the Rings" + Keys.ENTER);
+        Thread.sleep(2000);
         WebElement result2 = driver.findElement(By.xpath("//div[@id='result-stats']"));
         String[] resultArr2 = result2.getText().split(" ");
         System.out.println("Lord of the Rings ==> " + resultArr2[1] + " Sonuç Bulundu");
 
+        //-arama kutusuna  "Kill Bill" yazip, cikan sonuc sayisini yazdirin
+        WebElement thirdSearchBox = driver.findElement(By.xpath("(//textarea)[1]"));
+        thirdSearchBox.sendKeys("Kill Bill" + Keys.ENTER);
+        Thread.sleep(2000);
         WebElement result3 = driver.findElement(By.xpath("//div[@id='result-stats']"));
         String[] resultArr3 = result3.getText().split(" ");
         System.out.println("Kill Bill ==> " + resultArr3[1] + " Sonuç Bulundu");
 
-    }
-
-    @AfterClass
-    public static void afterClass() throws Exception {
         Thread.sleep(1500);
         driver.close();
     }
