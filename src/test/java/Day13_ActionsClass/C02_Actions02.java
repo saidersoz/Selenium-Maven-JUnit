80f5d3a4ac8807ac8c2b6faf8a6252a5c68f06d2
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-09 16:25:21 +0300

    09/04/2023

--- src/test/java/Day13_ActionsClass/C02_Actions02.java
+++ src/test/java/Day13_ActionsClass/C02_Actions02.java
@@ -1,10 +1,37 @@
 package Day13_ActionsClass;
 
-public class C02_Actions02 {
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
-    //Amazon anasayfasına gidin
-    //"Account & List" üzerinde bekleyin (mouse over - hover over)
-    //Açılan menüde "Account" linkine tıklayın
-    //Açılan sayfanın başlığının "Your Account" içerdiğini doğrulayın
+public class C02_Actions02 extends TestBase {
 
+    @Test
+    public void actions() {
+        //1 -- Amazon anasayfasına gidin
+        driver.get("https://amazon.com");
+        bekle(2);
+
+        //2 -- "Account & List" üzerinde bekleyin (mouse over - hover over)
+        WebElement accountList = driver.findElement(By.xpath("//div[@class='nav-line-1-container']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(accountList).perform();
+        //moveToElement method'u ile mause locate ettiğimiz WE'in üzerine gider
+        //actions'u sonlandırmak için perform() method'u kullanırız.
+        bekle(2);
+
+
+        //3 -- Açılan menüde "Account" linkine tıklayın
+        driver.findElement(By.xpath("//*[text()='Account']")).click();
+        bekle(2);
+
+
+        //4 -- Açılan sayfanın başlığının "Your Account" içerdiğini doğrulayın
+        Assert.assertEquals("Your Account", driver.getTitle());
+        bekle(2);
+
+    }
 }
