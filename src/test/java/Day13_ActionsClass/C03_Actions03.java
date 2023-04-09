908e0fef033089532b5793449c83ce8fe159f4ac
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-09 16:55:24 +0300

    09/04/2023

--- src/test/java/Day13_ActionsClass/C03_Actions03.java
+++ src/test/java/Day13_ActionsClass/C03_Actions03.java
@@ -1,4 +1,33 @@
 package Day13_ActionsClass;
 
-public class C03_Actions03 {
+import Utilities.TestBase;
+import org.checkerframework.checker.units.qual.Acceleration;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
+public class C03_Actions03 extends TestBase {
+
+    /*
+     https://techproeducation.com adresine gidin
+     Sayfanın alt tarafına gidin
+     Sayfanın üst tarafına gidin
+     */
+
+    @Test
+    public void actions() {
+        //https://techproeducation.com adresine gidin
+        driver.get("https://techproeducation.com");
+        driver.findElement(By.xpath("//i[@class='eicon-close']")).click(); bekle(2);
+
+        //Sayfanın alt tarafına gidin
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).
+                sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).perform();
+        bekle(2);
+
+        //Sayfanın üst tarafına gidin
+
+    }
 }
