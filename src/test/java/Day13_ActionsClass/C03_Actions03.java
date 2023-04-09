2b16be462c1ca6af0ec990cb33eb2d731f2ac453
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-09 17:31:56 +0300

    09/04/2023

--- src/test/java/Day13_ActionsClass/C03_Actions03.java
+++ src/test/java/Day13_ActionsClass/C03_Actions03.java
@@ -5,6 +5,7 @@ import org.checkerframework.checker.units.qual.Acceleration;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 import java.security.Key;
@@ -51,5 +52,18 @@ public class C03_Actions03 extends TestBase {
         bekle(2);
         actions.sendKeys(Keys.HOME).build().perform();
         //build() method'u ile birden fazla oluşturduğumuz action objesini birbirimizw bağlayabiliriz.
+    }
+
+    @Test
+    public void actions3() {
+        driver.get("https://amazon.com");
+        //ARAMA KUTUSUNA BÜTÜK HARFLERLE SAMSUNG YAZ
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys(Keys.SHIFT,"s",Keys.SHIFT,"amsung");
+        aramaKutusu.submit();
+
+        Actions actions = new Actions(driver);bekle(2);
+        actions.sendKeys(Keys.END); bekle(2);
+        actions.sendKeys(Keys.HOME).build().perform();bekle(1);
     }
 }
