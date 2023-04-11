959be3a6ddead482a832b973d0e92b947cab69db
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-12 01:57:07 +0300

    12/04/2023

--- src/test/java/Day15_FileUpload_Wait/C04_ExplicitWait.java
+++ src/test/java/Day15_FileUpload_Wait/C04_ExplicitWait.java
@@ -1,11 +1,37 @@
 package Day15_FileUpload_Wait;
 
-public class C04_ExplicitWait {
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class C04_ExplicitWait extends TestBase {
 
     /*
     https://the-internet.herokuapp.com/dynamic_controls sayfasına gidin
     Remove tuşuna basın
     "It's gone!" yazısını doğrulayın
      */
+
+    @Test
+    public void explicitWaitTest() {
+        //https://the-internet.herokuapp.com/dynamic_controls sayfasına gidin
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        bekle(2);
+
+        //Remove tuşuna basın
+        driver.findElement(By.xpath("(//button)[1]")).click();
+
+        //"It's gone!" yazısını doğrulayın
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='message']")));
+        Assert.assertTrue(element.isDisplayed());
+    }
+
 
 }
