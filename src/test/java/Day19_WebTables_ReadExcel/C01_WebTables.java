61db09c511c738816af7964e01f90d0e603cf1b8
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-23 22:54:02 +0300

    23/04/2023

---
+++ src/test/java/Day19_WebTables_ReadExcel/C01_WebTables.java
@@ -0,0 +1,23 @@
+package Day19_WebTables_ReadExcel;
+
+import Utilities.TestBase;
+import org.junit.Test;
+
+public class C01_WebTables extends TestBase {
+
+    //    https://the-internet.herokuapp.com/tables sayfasına gidin
+    //    Task 1 : Table1’i yazdırın
+    //    Task 2 : 3. Satır verilerini yazdırın
+    //    Task 3 : Son satırın verilerini yazdırın
+    //    Task 4 : 5. Sütun verilerini yazdırın
+    //    Task 5 : Iki parametreli bir Java metodu oluşturalım: printData
+    //    Parameter 1 = satır numarası
+    //    Parameter 2 = sütun numarası
+    //    printData(2,3);  => 2. satır, 3. sütundaki veriyi yazdırsın.
+
+
+    @Test
+    public void table1Print() {
+
+    }
+}
