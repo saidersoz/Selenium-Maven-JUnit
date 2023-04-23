4a9cd668d4b1c89765cc2dbe2941c886a684b91f
saidErsoz <m.said.ersoz3443@gmail.com>
2023-04-24 00:18:15 +0300

    24/04/2023

--- src/test/java/Day19_WebTables_ReadExcel/C01_WebTables.java
+++ src/test/java/Day19_WebTables_ReadExcel/C01_WebTables.java
@@ -2,6 +2,8 @@ package Day19_WebTables_ReadExcel;
 
 import Utilities.TestBase;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class C01_WebTables extends TestBase {
 
@@ -11,13 +13,22 @@ public class C01_WebTables extends TestBase {
         driver.get("https://the-internet.herokuapp.com/tables");
 
         //    Task 1 : Table1’i yazdırın
+        WebElement table1 = driver.findElement(By.xpath("(//table)[1]"));
+        System.out.println(table1.getText());
 
+        System.out.println();
 
         //    Task 2 : 3. Satır verilerini yazdırın
+        WebElement table1Satir3 = driver.findElement(By.xpath("//table[1]//tbody[1]//tr[3]"));
+        System.out.println(table1Satir3.getText());
 
+        System.out.println();
 
         //    Task 3 : Son satırın verilerini yazdırın
+        WebElement table2satir4 = driver.findElement(By.xpath("(//tbody)[2]//tr[4]"));
+        System.out.println(table2satir4.getText());
 
+        System.out.println();
 
         //    Task 4 : 5. Sütun verilerini yazdırın
 
